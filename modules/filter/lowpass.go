24c449cc069b368fb5313f1bae58b9ad4cfa9638
ajzaff <ajzaff@gmail.com>
2021-07-03 16:23:23 -0400

    Simple lowpass (still hear some pops)

--- modules/filter/lowpass.go
+++ modules/filter/lowpass.go
@@ -9,8 +9,13 @@ import (
 )
 
 type LowPass struct {
+	Cutoff func(i int) float32
+
 	blockSize int
 	rate      int
+
+	buffer []complex128
+	filter []complex128
 }
 
 func (f *LowPass) SetConfig(cfg *modular.Config) {
@@ -18,33 +23,40 @@ func (f *LowPass) SetConfig(cfg *modular.Config) {
 	f.rate = cfg.SampleRate
 }
 
-func (f *LowPass) computeFilter() []complex128 {
-	b := make([]complex128, f.blockSize) // FIXME: avoid allocations.
-	for i := range b {
-		t := -(float64(f.blockSize)-1)/2 + float64(i)
-		v := (2 * real(b[i]) / float64(f.rate)) * sincfn(2*real(b[i])*t/float64(f.rate))
-		b[i] = complex(v, 0)
-	}
-	for i, v := range window.Blackman(f.blockSize) {
-		b[i] *= complex(v, 0)
-	}
-	return b
+func sincfn(x float32) float32 {
+	return float32(math.Sin(math.Pi*float64(x)) / (math.Pi * float64(x)))
 }
 
-func sincfn(x float64) float64 {
-	return math.Sin(math.Pi*x) / (math.Pi * x)
+func (f *LowPass) computeFilter() {
+	if f.filter == nil {
+		f.filter = make([]complex128, f.blockSize)
+	}
+	for i := range f.filter {
+		c := f.Cutoff(i)
+		t := -(float32(f.blockSize)-1)/2 + float32(i)
+		v := (2 * c / float32(f.rate)) * sincfn(2*c*t/float32(f.rate))
+		f.filter[i] = complex(float64(v), 0)
+	}
+	for i, v := range window.Blackman(f.blockSize) {
+		f.filter[i] *= complex(v, 0)
+	}
 }
 
 func (f *LowPass) Process(b []float32) {
-	x := make([]complex128, len(b)) // FIXME: avoid allocations.
-	for i, v := range b {
-		x[i] = complex(float64(v), 0)
+	if len(b) != f.blockSize {
+		panic("filter.LowPass: Process called with wrong size block")
 	}
-	h := f.computeFilter()
-	y := fft.Convolve(x, h)
+	if f.buffer == nil {
+		f.buffer = make([]complex128, len(b))
+	}
+	for i, v := range b {
+		f.buffer[i] = complex(float64(v), 0)
+	}
+	f.computeFilter()
+	y := fft.Convolve(f.buffer, f.filter)
 
-	copy(x, y)
-	for i, v := range x {
+	copy(f.buffer, y)
+	for i, v := range f.buffer {
 		b[i] = float32(real(v))
 	}
 }
