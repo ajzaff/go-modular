371e87616aefd8bd83d3a473f319673c0920486f
ajzaff <ajzaff@gmail.com>
2021-07-06 17:19:38 -0400

    ADSR overhaul

---
+++ examples/modules/adsr/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"time"
+
+	"github.com/ajzaff/go-modular"
+	"github.com/ajzaff/go-modular/midi"
+	"github.com/ajzaff/go-modular/modules/adsr"
+	"github.com/ajzaff/go-modular/modules/osc"
+	"github.com/ajzaff/go-modular/modules/output/otoplayer"
+)
+
+func main() {
+	cfg := modular.New()
+
+	b := make([]float32, 5*44100)
+	for i := range b {
+		b[i] = 69. / 12
+	}
+
+	w := osc.Sine(.1, osc.Range16, osc.Fine(midi.StdTuning))
+	w.SetConfig(cfg)
+	w.Process(b)
+
+	g := adsr.New(time.Second, time.Second, .5, time.Second)
+	g.SetConfig(cfg)
+	g.SetSustain(time.Second)
+	g.Process(b)
+
+	oto := otoplayer.New()
+	oto.SetConfig(cfg)
+	oto.PlayStereo(b)
+}
--- modules/adsr/adsr.go
+++ modules/adsr/adsr.go
@@ -1,117 +1,138 @@
 package adsr
 
 import (
+	"math"
 	"time"
 
 	"github.com/ajzaff/go-modular"
 )
 
-type triggerState int
-
-const (
-	stateAwait triggerState = iota
-	stateAttack
-	stateDecay
-	stateSustain
-	stateRelease
-)
-
-// Envelope is a basic ADSR envelope generator.
+// ADSR is a basic ADSR envelope generator.
 //
 // Gate transitions shape the generated envelope:
 //	0->1: Attack trigger.
 //	1->1: Decay/Sustain.
 //	1->0: Release trigger.
 //	0->0: Off.
-type Envelope struct {
-	a, d       time.Duration
-	s          float32
-	r          time.Duration
-	buf        []float32
-	t          float32
-	len        float32
-	state      triggerState
+type ADSR struct {
+	a, d time.Duration
+	s    float32
+	r    time.Duration
+
+	phase   phase
+	begin   int
+	p       int
+	end     int
+	sustain int
+
 	sampleRate int
 }
 
-func New(a time.Duration, d time.Duration, s float32, r time.Duration) *Envelope {
-	return &Envelope{
-		a,
-		d,
-		s,
-		r,
-		nil,
-		0,
-		0,
-		stateAwait,
-		44100,
+func New(a time.Duration, d time.Duration, s float32, r time.Duration) *ADSR {
+	adsr := &ADSR{
+		a:          a,
+		d:          d,
+		s:          s,
+		r:          r,
+		sampleRate: 44100,
 	}
+	adsr.Reset()
+	return adsr
 }
 
-func (e *Envelope) UpdateConfig(cfg *modular.Config) error {
-	e.sampleRate = cfg.SampleRate
+func (a *ADSR) SetConfig(cfg *modular.Config) error {
+	a.sampleRate = cfg.SampleRate
+	a.Reset()
 	return nil
 }
 
-func (e *Envelope) Gate() modular.Processor {
-	panic("adsr.Envelope.Gate: not implemented")
+type phase int
+
+const (
+	attack phase = iota
+	decay
+	sustain
+	release
+)
+
+func (a *ADSR) samples(d time.Duration) int {
+	return int(math.Round(float64(a.sampleRate) * float64(d.Seconds())))
 }
 
-func length(sampleRate int, d time.Duration) float32 {
-	if d == 0 {
-		return 0
+// Reset resets the ADSR to the attack phase.
+func (a *ADSR) Reset() {
+	a.phase = attack
+	a.begin = 0
+	a.p = 0
+	a.end = a.samples(a.a)
+}
+
+// SetSustain optionally sets a fixed duration for the sustain phase.
+//
+// The duration d should not include attack and decay.
+func (a *ADSR) SetSustain(d time.Duration) {
+	a.sustain = a.samples(a.a + a.d + d)
+}
+
+// Release releases the note now if held.
+func (a *ADSR) Release() {
+	if a.phase == sustain {
+		a.releaseNow()
 	}
-	return float32(sampleRate) * float32(d.Seconds())
 }
 
-func (e *Envelope) Read(vs []float32) (n int, err error) {
-	n = copy(vs, e.buf)
-	for i, v := range vs[:n] {
-		switch e.state {
-		case stateAwait:
-			if v > 0 {
-				e.state = stateAttack
-				e.t = 0
-				e.len = length(e.sampleRate, e.a)
-			}
-			vs[i] = 0
-		case stateAttack:
-			if v <= 0 {
-				e.state = stateRelease
-				e.t = 0
-				e.len = length(e.sampleRate, e.r)
-				continue
-			}
-		case stateDecay:
-			if v <= 0 {
-				e.state = stateRelease
-				e.t = 0
-				e.len = length(e.sampleRate, e.r)
-				continue
-			}
-		case stateSustain:
-			if v <= 0 {
-				e.state = stateRelease
-				e.t = 0
-				e.len = length(e.sampleRate, e.r)
-				continue
-			}
-			vs[i] = float32(e.s)
-		case stateRelease:
-			if v > 0 {
-				e.state = stateAttack
-				e.t = 0
-				e.len = length(e.sampleRate, e.a)
-				continue
-			}
-			vs[i] = float32(e.s - e.s*e.t/e.len)
-			e.t++
-			if e.len < e.t+1 {
-				e.state = stateAwait
-				e.t = 0
-				e.len = 0
-			}
+func (a *ADSR) releaseNow() {
+	a.phase = release
+	a.begin = a.p
+	a.end = a.p + a.samples(a.r)
+}
+
+// Envelope returns the next envelope amplitude.
+//
+// Envelope calls mutate the ADSR.
+func (a *ADSR) Envelope() float32 {
+	switch a.phase {
+	case attack:
+		if a.p >= a.end {
+			a.phase = decay
+			a.begin = a.p
+			a.end = a.p + a.samples(a.d)
+			return 1
 		}
+		defer func() { a.p++ }()
+		// TODO: Configurable attackRamp func.
+		return float32(a.p-a.begin) / float32(a.end-a.begin)
+	case decay:
+		if a.p >= a.end {
+			a.phase = sustain
+			a.begin = -1
+			a.end = -1
+			return a.s
+		}
+		defer func() { a.p++ }()
+		return 1 - a.s*float32(a.p-a.begin)/float32(a.end-a.begin)
+	case sustain:
+		if a.sustain != 0 && a.p >= a.sustain {
+			a.releaseNow()
+			return a.s
+		}
+		a.p++
+		return a.s
+	case release:
+		if a.p >= a.end {
+			return 0
+		}
+		defer func() { a.p++ }()
+		// TODO: Configurable release ramp.
+		return a.s - a.s*float32(a.p-a.begin)/float32(a.end-a.begin)
+	default:
+		panic("ADSR.Envelope: impossible state reached")
 	}
-	return n, err
+}
+
+// Process convolves the block with the ADSR envelope.
+func (a *ADSR) Process(b []float32) {
+	for i, v := range b {
+		b[i] = v * a.Envelope()
+	}
 }
